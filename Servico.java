8794f2991b0cd13078029aabb60438714f74a449
Samuel Covalski <samuelcovalski@gmail.com>
2026-09-04 20:28:15 -0300

    refactor: extrai classe Servico

---
+++ Servico.java
@@ -0,0 +1,23 @@
+public class Servico {
+    private String nome;
+    private double preco;
+    private String descricao;
+
+    public Servico(String nome, double preco, String descricao) {
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
